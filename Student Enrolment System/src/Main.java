89389c96b0b07cce130e980776423bc9c3cb4324
s3878023 <73817832+s3878023@users.noreply.github.com>
2022-03-25 19:48:51 +0700

    Add files via upload

--- Student Enrolment System/src/Main.java
+++ Student Enrolment System/src/Main.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String args[]) {
+        Scanner studentInputID = new Scanner(System.in);
+        Scanner enrollSemester = new Scanner(System.in);
+        Scanner courseInputID = new Scanner(System.in);
+        System.out.println("Welcome to the Student Enrolment System");
+
+        System.out.println("Please Enter The Student ID:");
+        String studentIDObj = studentInputID.nextLine();
+
+        System.out.println("Please Enter the Semester:");
+        String enrollSemesterObj = enrollSemester.nextLine();
+
+        System.out.println("Please Enter The Course ID");
+        String courseIDObj = courseInputID.nextLine();
+
+
+        System.out.println("Confirm your choice as listed:"+ " " + "Student ID: " + studentIDObj+ " " + "Semester: " + enrollSemesterObj + " " + "Course ID: " + courseIDObj);
+        System.out.println("Confirm your enrolment ?   Y/N ");
+
+        Student s1 = new Student("","","");
+
+    }
 }
--- Student Enrolment System/src/StudentEnrolment.java
+++ Student Enrolment System/src/StudentEnrolment.java
@@ -7,11 +7,5 @@ public class StudentEnrolment {
 
     }
 
-    public static void main(String args[]) {
-        System.out.println("Welcome to the Student Enrolment System");
-        System.out.println("Please Enter The Student Name:");
 
-        Student s1 = new Student("","","");
-
-    }
 }
