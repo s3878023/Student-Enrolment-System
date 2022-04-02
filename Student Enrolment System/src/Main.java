85cfb751be7d7568051ff0c540f7b626194b8dce
s3878023 <73817832+s3878023@users.noreply.github.com>
2022-04-02 16:00:37 +0700

    Delete Student Enrolment System directory

--- Student Enrolment System/src/Main.java
+++
@@ -1,7 +0,0 @@
-class Main {
-    public static void main(String[] args){
-        StudentEnrolmentManager manager = new StudentEnrolmentManager();
-        manager.add();
-        manager.getAll();
-    }
-}
